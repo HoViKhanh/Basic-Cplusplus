7824931562486eb96b289697b688d8c99fd69cf8
HoViKhanh <vikhanhho@gmail.com>
2025-04-14 22:10:45 +0700

    update OOP/BaiHoc11

---
+++ OOP/BaiHoc11/README.md
@@ -0,0 +1,6 @@
+# ĐA XẠ - ĐA HÌNH - POLYMORPHISM
+![img.png](image/img.png)![img_5.png](image/img_5.png)
+![img_1.png](image/img_1.png)
+![img_2.png](image/img_2.png)
+![img_3.png](image/img_3.png)
+![img_4.png](image/img_4.png)
---
+++ OOP/BaiHoc11/vidu2.cpp
@@ -0,0 +1,49 @@
+//
+// Created by ViKa on 4/14/2025.
+//
+#include <iostream>
+
+using namespace std;
+
+class A {
+public:
+    virtual void Skeychy(){
+        cout<<"\nA's Skeychy()";
+        Skeychy(-1);
+    }
+    void Skeychy(int num){
+        cout<<"\nA's Skeychy("<<num<<")";
+    }
+};
+class B:public A {
+public:
+    void Skeychy(){
+        cout<<"\nB's Skeychy()";
+        Skeychy(-2);
+    }
+    void Skeychy(int num){
+        cout<<"\nB's Skeychy("<<num<<")";
+    }
+};
+class C:public B {
+public:
+    void Skeychy(int num){
+        cout<<"\nC's Skeychy("<<num<<")";
+    }
+};
+void Curious(A* wacky){
+    wacky->Skeychy();
+    ((C*)wacky)->Skeychy(123);
+};
+int main() {
+    A* inky = new B;
+    inky->Skeychy();
+    inky->Skeychy(23);
+    Curious(inky);
+
+    B* pinky = new C;
+    pinky->Skeychy();
+    pinky->Skeychy(46);
+    Curious(pinky);
+    return 0;
+}
